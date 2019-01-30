e70722521429b2640dbb774853c812783ea95a97
Andrew Kovalenko <konderson97@gmail.com>
2019-01-30 17:52:52 +0600

    Last Change from ofice 30.01.2019

--- Models/News.php
+++ Models/News.php
@@ -18,26 +18,28 @@ class News
 
         $host='127.0.0.1';
         $database="basei";
-        $user='t';
-        $password='root';
-        $db = new PDO('mysql:host=127.0.0.1;dbname=mvc_site', $user, $password);
+        $user='konderson';
+        $password='220831';
 
-$newList=array();
-$result=$db->query('SELECT id,title,date,short_conent'
+        $db = new PDO('mysql:host=127.0.0.1;dbname=mvc_site', $user, $password,[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+$newsList=array();
+$result=$db->query('SELECT *'
 .'FROM news '
-    .' ORDER BY data DESC'
+    .'ORDER BY data DESC '
     .'LIMIT 10');
 
 $i=0;
 while ($row=$result->fetch()){
+
     $newsList[$i]['id']=$row['id'];
-    $newList[$i]['title']=$row['title'];
-    $newList[$i]['date']=$row['data'];
-    $newList[$i]['short_content']=$row['short_content'];
+    $newsList[$i]['title']=$row['title'];
+    $newsList[$i]['date']=$row['data'];
+    $newsList[$i]['short_content']=$row['short_content'];
     $i++;
 }
 
-        return $newList;
+        return $newsList;
 }
 
 }
