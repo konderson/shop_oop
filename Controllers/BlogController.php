1798ce24277cdf2c9681b13c82391cab5839c9a5
Kovalenko_Notnik <konderson97@gmail.com>
2019-02-01 00:14:55 +0300

    Begin Eshoper

---
+++ Controllers/BlogController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Notnik_kg
+ * Date: 31.01.2019
+ * Time: 23:31
+ */
+
+class BlogController
+{
+
+}
---
+++ Controllers/SiteController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Notnik_kg
+ * Date: 31.01.2019
+ * Time: 23:31
+ */
+
+class SiteController
+{
+public  function  actionIndex(){
+
+   require_once (ROOT.'/Views/site/index.php');
+}
+}
